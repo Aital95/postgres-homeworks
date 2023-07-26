5c7f221768ba9b06b7d0f08a72940b5d0c170369
Aital95 <128999628+Aital95@users.noreply.github.com>
2023-07-27 03:20:02 +0600

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,11 +1,10 @@
--- SQL-команды для создания таблиц
-
 CREATE TABLE employees (
     employee_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    position VARCHAR(100),
-    department VARCHAR(100)
+    job_title VARCHAR(100),
+    department VARCHAR(100),
+    hire_date DATE
 );
 
 CREATE TABLE customers (
@@ -13,13 +12,14 @@ CREATE TABLE customers (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100),
-    phone VARCHAR(20)
+    phone VARCHAR(20),
+    address VARCHAR(200)
 );
 
 CREATE TABLE orders (
     order_id SERIAL PRIMARY KEY,
+    order_date DATE NOT NULL,
     customer_id INTEGER REFERENCES customers(customer_id),
     employee_id INTEGER REFERENCES employees(employee_id),
-    order_date DATE,
-    total_amount NUMERIC(10, 2)
+    total_amount NUMERIC(10, 2) NOT NULL
 );
