eceddec7a04e8a5e5173c15d676bf53e9cc95255
Aital95 <128999628+Aital95@users.noreply.github.com>
2023-07-27 01:54:42 +0600

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    position VARCHAR(100),
+    department VARCHAR(100)
+);
+
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(customer_id),
+    employee_id INTEGER REFERENCES employees(employee_id),
+    order_date DATE,
+    total_amount NUMERIC(10, 2)
+);
