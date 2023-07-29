74f150d2ddbf143b2c96ba46061d04906bf83b02
Айтал Емельянов <aytem95@gmail.com>
2023-07-30 01:16:46 +0600

    fixmain

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,25 +1,25 @@
-CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    job_title VARCHAR(100),
-    department VARCHAR(100),
-    hire_date DATE
+CREATE TABLE customers_data
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
 );
 
-CREATE TABLE customers (
-    customer_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100),
-    phone VARCHAR(20),
-    address VARCHAR(200)
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(100),
+	birth_date date NOT NULL,
+	notes text
 );
 
-CREATE TABLE orders (
-    order_id SERIAL PRIMARY KEY,
-    order_date DATE NOT NULL,
-    customer_id INTEGER REFERENCES customers(customer_id),
-    employee_id INTEGER REFERENCES employees(employee_id),
-    total_amount NUMERIC(10, 2) NOT NULL
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50)
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -7,34 +7,31 @@ def main():
     try:
         with conn:
             with conn.cursor() as cur:
-                with open('C:/Users/Aital/postgres-homeworks/homework-1/north_data/customers_data.csv', 'r', newline='') as csvfile:
+                with open('north_data/customers_data.csv', 'r', newline='') as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
                         cur.execute(
-                            f'INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)',
+                            'INSERT INTO customers_data (customer_id, company_name, contact_name) VALUES (%s, %s, %s)',
                             (row["customer_id"], row["company_name"], row["contact_name"]))
 
-                with open('C:/Users/Aital/postgres-homeworks/homework-1/north_data/employees_data.csv', 'r', newline='') as csvfile:
+                with open('north_data/employees_data.csv', 'r', newline='') as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
                         cur.execute(
-                            f'INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes)'
-                            f' VALUES (%s, %s, %s, %s, %s, %s)',
-                            (row["employee_id"], row["first_name"], row["last_name"], row["title"], row["birth_date"],
-                             row["notes"]))
-                #
-                with open('C:/Users/Aital/postgres-homeworks/homework-1/north_data/orders_data.csv', 'r', newline='') as csvfile:
+                            'INSERT INTO employees_data (employee_id, first_name, last_name, title, birth_date, notes)'
+                            ' VALUES (%s, %s, %s, %s, %s, %s)',
+                            (row["employee_id"], row["first_name"], row["last_name"], row["title"], row["birth_date"], row["notes"]))
+
+                with open('north_data/orders_data.csv', 'r', newline='') as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
                         cur.execute(
-                            f'INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city)'
-                            f' VALUES (%s, %s, %s, %s, %s)',
-                            (row["order_id"], row["customer_id"], row["employee_id"], row['order_date'],
-                             row['ship_city']))
+                            'INSERT INTO orders_data (order_id, customer_id, employee_id, order_date, ship_city)'
+                            ' VALUES (%s, %s, %s, %s, %s)',
+                            (row['order_id'], row["customer_id"], row["employee_id"], row['order_date'], row['ship_city']))
 
     finally:
         conn.close()
-
 
 if __name__ == '__main__':
     main()
